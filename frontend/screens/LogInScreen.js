631465de355e2cda55bff35b9152ecacf1ad5bfa
Le Cong Nam <lecongnamgb@gmail.com>
2022-05-08 15:47:16 +0700

    add logIn feature

--- frontend/screens/LogInScreen.js
+++ frontend/screens/LogInScreen.js
@@ -9,8 +9,15 @@ import NormalField from '../components/checkInComponents/NormalField'
 import PasswordField from '../components/checkInComponents/PasswordField'
 
 export default function LogInScreen() {
-    const [showPassword, setShowpassword] = useState(false)
+    const listData = [
+        {
+            username: 'lecongnam',
+            password: '123456'
+        },
+    ]
     const navigation = useNavigation()
+    const [username, setUsername] = useState();
+    const [password, setPassword] = useState();
 
   return (
     <SafeAreaView style = {{height: '100%', backgroundColor: 'white'}}>
@@ -33,10 +40,37 @@ export default function LogInScreen() {
       </View>
       <View>
           <View style = {[{margin: 25}]}>
-                <NormalField plhdTitle = "Nhập tên đăng nhập" sourceIcon = {require('../assets/icon/account_icon.png')} focus = {true}/>
-                <PasswordField plhdTitle = "Mật khẩu"/>
+                <NormalField 
+                    plhdTitle = "Nhập tên đăng nhập" 
+                    sourceIcon = {require('../assets/icon/account_icon.png')} 
+                    focus = {true}
+                    value = {username}
+                    handleOnChangeText = {text => setUsername(text)}
+                />
+                <PasswordField 
+                    plhdTitle = "Mật khẩu"
+                    value = {password}
+                    handleOnChangeText = {text => setPassword(text)}
+                />
           </View>
-          <TouchableOpacity style = {[{backgroundColor: '#ff8000', height: 40, marginLeft: 25, marginRight: 25}, styles.alignCenterItemVertically]}>
+          <TouchableOpacity 
+            style = {[{backgroundColor: '#ff8000', height: 40, marginLeft: 25, marginRight: 25}, styles.alignCenterItemVertically]}
+            onPress = {() => {
+                const doesExist = false;
+                listData.map(userAccount => {
+                    if (userAccount.username === username && userAccount.password === password) {
+                        doesExist = true;
+                        alert('Đăng nhập thành công')
+                        navigation.navigate('Home')
+                        return true;
+                    } 
+                })
+                if (doesExist === false) {
+                    alert('Tài khoản hoặc mật khẩu không đúng')
+                }
+                
+            }}
+            >
             <Text style = {{textAlign: 'center', color: '#fff', fontSize: 18}}>Đăng nhập</Text>
           </TouchableOpacity>
           <View style = {[styles.ml_25, styles.mr_25, styles.mt_20, styles.flex_row, {justifyContent: 'flex-end'}]}>
